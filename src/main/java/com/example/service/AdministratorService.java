57363491e2d0acfe5b24549d60a5f18725f0148a
tomoki <sugisugi0514@gmail.com>
2024-05-14 13:48:16 +0900

    secondCommit

--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.domain.Administrator;
 import com.example.repository.AdministratorRepository;
 
 @Service
@@ -15,4 +16,12 @@ import com.example.repository.AdministratorRepository;
 public class AdministratorService {
     @Autowired
     private AdministratorRepository administratorRepository;
+
+    /*
+     * 管理者情報を挿入する
+     * administratorRepositryのinsert()メソッドを呼ぶ
+     */
+    public void insert(Administrator administrator) {
+        administratorRepository.insert(administrator);
+    }
 }
